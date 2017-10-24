6aae4fc96ae9dfeba024d10ac031f35fc3017633
Juan Pablo Marengo <juanpmarengo@gmail.com>
2017-10-24 18:53:13 -0300

    Assert lib included, rmdir libs, gotoxy definition

--- src/main/letterSoup.c
+++ src/main/letterSoup.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void goToXY (int x, int y) {
+    printf("%c[%d;%df", 0x1B, y, x);
+}
+
+void clearScreen () {
+    system("clear");
+}
 
 int main () {
-    printf("Hello World\n");
+    clearScreen ();
+
+    goToXY (4, 5);
+    printf ("Hello world!\n");
 
     return 0;
 }
