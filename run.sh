f6ecf19864981c3240786cc12b84eecbdb7529c2
Juan Pablo Marengo <juanpmarengo@gmail.com>
2017-10-24 18:10:05 -0300

    Make 'build' dir if not exists

--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 # Run script
 
-if [ "$1" = "test" ]
-then
+if [ ! -d "build" ]; then
+    mkdir build
+fi
+
+if [ "$1" = "test" ]; then
     gcc ./src/test/letterSoupTest.c -o build/output && ./build/output
 else
     gcc ./src/main/letterSoup.c -o build/output && ./build/output
