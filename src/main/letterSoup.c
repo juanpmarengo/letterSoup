fdf54745f60ce0b25c2a5b3a9c85a71853d03111
Juan Pablo Marengo <juanpmarengo@gmail.com>
2017-10-24 21:37:20 -0300

    Grid reader

--- src/main/letterSoup.c
+++ src/main/letterSoup.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
+#include <string.h>
+
+struct Grid {
+    int width;
+    int height;
+    char **rows;
+};
 
 void goToXY (int x, int y) {
     printf("%c[%d;%df", 0x1B, y, x);
@@ -9,11 +17,54 @@ void clearScreen () {
     system("clear");
 }
 
-int main () {
-    clearScreen ();
+struct Grid readGrid (char *path) {
+    char str[100], **ptr;
+    int strLen;
+    struct Grid grid;
 
-    goToXY (4, 5);
-    printf ("Hello world!\n");
+    grid.height = 0;
+    grid.width = INT_MAX;
+    grid.rows = NULL;
+
+    FILE * file;
+    file = fopen(path , "r");
+
+    if (file) {
+        while ((fscanf(file, "%s", str)) != EOF) {
+            // Realloc the first dimension
+            ptr = realloc(grid.rows, (grid.height + 1) * sizeof(char *));
+
+            if (ptr) {
+                grid.rows = ptr;
+                grid.height++;
+
+                // Save row
+                strLen = strlen(str);
+
+                if (grid.width > strLen) {
+                    grid.width = strLen;
+                }
+
+                grid.rows[grid.height - 1] = malloc(grid.width * sizeof(char));
+                strcpy(grid.rows[grid.height - 1], str);
+            }
+        }
+
+        fclose(file);
+    }
+
+    return grid;
+}
+
+int main () {
+    struct Grid grid = readGrid("grid.txt");
+
+    printf("%d\n", grid.width);
+    printf("%d\n", grid.height);
+
+    for (int x = 0; x < grid.height; x++) {
+        printf("%s\n", grid.rows[x]);
+    }
 
     return 0;
 }
